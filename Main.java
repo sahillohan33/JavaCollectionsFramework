3e4e58b39a1af2d2be3460a25e70da318e75bac7
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-20 22:16:51 +0530

    Main Class

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        System.out.println(a[8]);
+        System.out.println("Hello Guys");
+    }
+}
