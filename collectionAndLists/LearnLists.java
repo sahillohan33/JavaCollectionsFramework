792f94165d9d53f53869f9fe257da5686dfed935
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-07-23 22:55:56 +0530

    Update list

--- collectionAndLists/LearnLists.java
+++ collectionAndLists/LearnLists.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class LearnLists {
     public static void main(String[] args) {
-      //  int a[]=new int[5];
 
+        /*
         List<Integer> list=new ArrayList<Integer>();
         List<Integer> list2=new ArrayList<>();
 
@@ -26,7 +26,7 @@ public class LearnLists {
         System.out.println(list);
         System.out.println(list2);
         // list.addAll(list2);
-        /*
+
         System.out.println(list.size());
         System.out.println(list.contains(30));
         list.remove(3);
@@ -36,15 +36,13 @@ public class LearnLists {
         list.removeAll(list2);
 
         System.out.println(list);
-*/
-        /*
+
         list.retainAll(list2);
         System.out.println(list);
 
         list.clear();
         System.out.println(list);
-        */
-/*
+
     Object a[]=list.toArray();
     for(Object e:a){
         Integer temp=(Integer)e;
@@ -53,6 +51,32 @@ public class LearnLists {
         System.out.println(list);
     */
 
+        List<Integer> list=new ArrayList<>();
+
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        list.add(1);
+        System.out.println(list);
+
+        System.out.println(list.get(0));
+        System.out.println(list.set(1,200));
+        System.out.println(list);
+        list.add(3,1000);
+        System.out.println(list);
+        list.remove(4); // 40
+
+        list.remove(Integer.valueOf(1)); // 1
+
+        System.out.println(list);
+
+        System.out.println(list.indexOf(200));
+
+        System.out.println(list.lastIndexOf(1));
 
 
     }
